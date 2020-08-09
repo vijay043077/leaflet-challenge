7443421a002b1d51cb8a66c6849a6a17c2ac4168
Vijay S <37852613+vijay043077@users.noreply.github.com>
2020-08-08 21:06:58 -0500

    commit2

---
+++ leaflet-step-1/static/js/config.js
@@ -0,0 +1,1 @@
+const API_KEY = "pk.eyJ1IjoidmVlai03NyIsImEiOiJja2RsNGttcmQwNGpmMnFxb2pweWt0MGZ0In0.aPsvMbQdIwvDFP5NxVHwYQ";
---
+++ leaflet-step-1/static/js/logic.js
@@ -0,0 +1,68 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function (data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+            "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    var earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature
+    });
+
+    // Sending our earthquakes layer to the createMap function
+    createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+    // Define streetmap and darkmap layers
+    var usMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    });
+
+
+
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+        "Street Map": usMap,
+    };
+
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+        Earthquakes: earthquakes
+    };
+
+    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    var myMap = L.map("map", {
+        center: [
+            37.09, -95.71
+        ],
+        zoom: 3,
+        layers: [usMap, earthquakes]
+    });
+
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+}
